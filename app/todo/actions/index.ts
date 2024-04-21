856f3c4e9c51709b57c95fc5f2db7b9210866b5c
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-22 01:02:46 +0700

    CRUD read

--- app/todo/actions/index.ts
+++ app/todo/actions/index.ts
@@ -1,16 +1,23 @@
 "use server";
 
 import createSupabaseServerClient from "@/lib/supabase/server";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
 
 export async function createTodo(title: string) {
     const supabase = await createSupabaseServerClient();
-
+    
     const result = await supabase.from("todo-demo").insert({title}).single();
-
+    revalidatePath("/todo");
+    
     return JSON.stringify(result);
 }
 
-export async function readTodo() {}
+export async function readTodo() {
+    noStore();
+    const supabase = await createSupabaseServerClient();
+
+    return await supabase.from("todo-demo").select("*");
+}
 
 export async function deleteTodoById(id: string) {}
 
